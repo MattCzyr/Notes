eb20701f9588d66912e61f8b65353792741aa262
mattczyr <mattczyr@gmail.com>
2017-01-20 21:15:20 -0500

    Account for ending newlines when wrapping a string

--- src/main/java/com/chaosthedude/notes/util/StringUtils.java
+++ src/main/java/com/chaosthedude/notes/util/StringUtils.java
@@ -32,13 +32,7 @@ public final class StringUtils {
 			}
 		}
 
-		if (!temp.isEmpty()) {
-			strings.add(temp);
-		}
-
-		if (strings.isEmpty()) {
-			strings.add("");
-		}
+		strings.add(temp);
 
 		return strings;
 	}
