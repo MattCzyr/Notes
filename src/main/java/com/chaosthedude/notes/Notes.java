1b54c73f9ad32121b2cbdb932a3c3f39d08eb057
mattczyr <mattczyr@gmail.com>
2017-01-21 21:37:52 -0500

    Increment version number

--- src/main/java/com/chaosthedude/notes/Notes.java
+++ src/main/java/com/chaosthedude/notes/Notes.java
@@ -19,7 +19,7 @@ public class Notes {
 
 	public static final String MODID = "notes";
 	public static final String NAME = "Notes";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 
 	public static final Logger logger = LogManager.getLogger(MODID);
 
