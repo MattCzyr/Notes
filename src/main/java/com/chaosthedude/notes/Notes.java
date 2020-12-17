5985ddd9b05c7d4c9cf55262948f83d19c23f921
Matt Czyr <mattczyr@gmail.com>
2020-12-16 22:59:55 -0500

    Fix case on English translation file, bump version, and update changelog

--- src/main/java/com/chaosthedude/notes/Notes.java
+++ src/main/java/com/chaosthedude/notes/Notes.java
@@ -18,7 +18,7 @@ public class Notes {
 
 	public static final String MODID = "notes";
 	public static final String NAME = "Notes";
-	public static final String VERSION = "1.1.2";
+	public static final String VERSION = "1.2.2";
 
 	public static final Logger logger = LogManager.getLogger(MODID);
 
