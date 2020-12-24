3b0baab43aaa3c3cb1153552bc2f05ce5303913b
Matt Czyr <mattczyr@gmail.com>
2020-12-24 17:41:11 -0500

    Allow mod to be installed on server without crashing and fix incompatibility marker when not installed on server (#11, #17)

--- src/main/java/com/chaosthedude/notes/Notes.java
+++ src/main/java/com/chaosthedude/notes/Notes.java
@@ -1,5 +1,6 @@
 package com.chaosthedude.notes;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,26 +9,34 @@ import com.chaosthedude.notes.event.RenderTickHandler;
 import com.chaosthedude.notes.key.KeybindHandler;
 import com.chaosthedude.notes.note.Note;
 
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Mod(Notes.MODID)
 public class Notes {
 
 	public static final String MODID = "notes";
 	public static final String NAME = "Notes";
-	public static final String VERSION = "1.2.2";
+	public static final String VERSION = "1.2.3";
 
 	public static final Logger logger = LogManager.getLogger(MODID);
 
 	public static Note pinnedNote;
 	
 	public Notes() {
-		MinecraftForge.EVENT_BUS.register(new KeybindHandler());
-		MinecraftForge.EVENT_BUS.register(new RenderTickHandler());
-		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ConfigHandler.CLIENT_SPEC);
+		DistExecutor.runWhenOn(Dist.CLIENT, () ->  () -> {
+			MinecraftForge.EVENT_BUS.register(new KeybindHandler());
+			MinecraftForge.EVENT_BUS.register(new RenderTickHandler());
+			ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ConfigHandler.CLIENT_SPEC);
+		});
+		
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 	}
 
 }
