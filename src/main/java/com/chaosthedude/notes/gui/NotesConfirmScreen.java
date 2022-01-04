01dd9094ee54a16e346a1347e2cb660e7b951bd3
Matt Czyr <mattczyr@gmail.com>
2022-01-03 19:18:23 -0500

    Fix button names in confirmation screen

--- src/main/java/com/chaosthedude/notes/gui/NotesConfirmScreen.java
+++ src/main/java/com/chaosthedude/notes/gui/NotesConfirmScreen.java
@@ -3,28 +3,25 @@ package com.chaosthedude.notes.gui;
 import it.unimi.dsi.fastutil.booleans.BooleanConsumer;
 import net.minecraft.client.gui.screens.ConfirmScreen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 
 public class NotesConfirmScreen extends ConfirmScreen {
 	
 	private BooleanConsumer callbackFunction;
-	private Component confirmButtonLabel;
-	private Component cancelButtonLabel;
 
 	public NotesConfirmScreen(BooleanConsumer callbackFunction, Component confirmButtonLabel, Component cancelButtonLabel) {
 		super(callbackFunction, confirmButtonLabel, cancelButtonLabel);
 		this.callbackFunction = callbackFunction;
-		this.confirmButtonLabel = confirmButtonLabel;
-		this.cancelButtonLabel = cancelButtonLabel;
 	}
 
 	@Override
 	protected void init() {
 		super.init();
 		clearWidgets();
-		addRenderableWidget(new NotesButton(this.width / 2 - 155, height / 6 + 96, 150, 20, confirmButtonLabel, (p_213002_1_) -> {
+		addRenderableWidget(new NotesButton(this.width / 2 - 155, height / 6 + 96, 150, 20, new TranslatableComponent("gui.yes"), (button) -> {
 			callbackFunction.accept(true);
 		}));
-		addRenderableWidget(new NotesButton(this.width / 2 - 155 + 160, height / 6 + 96, 150, 20, cancelButtonLabel, (p_213001_1_) -> {
+		addRenderableWidget(new NotesButton(this.width / 2 - 155 + 160, height / 6 + 96, 150, 20, new TranslatableComponent("gui.no"), (button) -> {
 			callbackFunction.accept(false);
 		}));
 	}
