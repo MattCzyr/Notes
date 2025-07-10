ee7c3c2c86f4cf7952f05c12780a9137e2887928
Matthew Czyr <mattczyr@gmail.com>
2025-07-10 16:33:56 -0400

    Fix render position of text after cursor in title box

--- src/main/java/com/chaosthedude/notes/gui/NotesTitleField.java
+++ src/main/java/com/chaosthedude/notes/gui/NotesTitleField.java
@@ -55,7 +55,7 @@ public class NotesTitleField extends EditBox {
 			if (!s.isEmpty()) {
 				String s1 = flag ? s.substring(0, j) : s;
 				guiGraphics.drawString(font, s1, l, i1, i, true);
-				j1 += font.width(s1);
+				j1 += font.width(s1) + 1;
 			}
 
 			boolean flag2 = getCursorPosition() < getValue().length() || getValue().length() >= pseudoMaxStringLength;
